377c2b17255e4347105688b0f3ad3a747eadaf0d
aTze91 <marcoatzeno@gmail.com>
2022-06-12 20:03:02 +0200

    test commands

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
-import markdown
+
 __winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
 __human_name__ = "templates"
 
 app = Flask(__name__)
-markdown.Markdown()
+
 
 
 @app.route("/")
 def index():
-    with open('README.md') as md_file:
-        mkd_text = md_file
-    return render_template("base.html", title='Index',mkd_text=mkd_text)
+    return render_template("base.html", title='Index')
 
 @app.route("/about")
 def about():
